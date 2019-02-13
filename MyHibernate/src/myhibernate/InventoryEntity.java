6d753ef5dcd602ea8b8a382f5d58f79a38606933
unknown <j.sterling.wright@gmail.com>
2019-02-13 01:04:04 -0500

    Initial Commit of hibernate

---
+++ MyHibernate/src/myhibernate/InventoryEntity.java
@@ -0,0 +1,45 @@
+package myhibernate;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
+@Table(name = "inventory", schema = "hibernate", catalog = "")
+public class InventoryEntity {
+    private int id;
+    private String description;
+
+    @Id
+    @Column(name = "id", nullable = false)
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Basic
+    @Column(name = "description", nullable = true, length = 128)
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        InventoryEntity that = (InventoryEntity) o;
+        return id == that.id &&
+                Objects.equals(description, that.description);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, description);
+    }
+}
