4463ba803b91f34866d6dc72e0d723dcad3475d3
Wright-Sterling <j.sterling.wright@gmail.com>
2019-04-10 13:19:14 -0400

    Cleaned up Servlet.java

--- MyQuiz360/src/quizServlet/Servlet.java
+++ MyQuiz360/src/quizServlet/Servlet.java
@@ -1,32 +1,24 @@
 package quizServlet;
 
 import javax.servlet.annotation.WebServlet;
-//import java.io.IOException;
-import java.io.PrintWriter;
-
-/* From hibernate example */
 import org.hibernate.HibernateException;
 import org.hibernate.Metamodel;
 import org.hibernate.query.Query;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
-import quiz360.QuestionEntity;
-
 import javax.persistence.metamodel.EntityType;
-
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import quiz360.QuestionEntity;
 
 
 @WebServlet(name = "Servlet", urlPatterns = ("/Servlet"))
 public class Servlet extends javax.servlet.http.HttpServlet {
 
-    /* From Hibernate example */
     private static final SessionFactory ourSessionFactory;
 
     static {
@@ -47,18 +39,10 @@ public class Servlet extends javax.servlet.http.HttpServlet {
     protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
         PrintWriter out = response.getWriter();
         response.setContentType("text/html");
-    /*    out.println("<html><head></head><body>"); */
-        String login = request.getParameter("login");
-        String password = request.getParameter("password");
-        out.println("<h1>Super secret login information</h1>");
-        out.println("<p>login: " + login + "</p>");
-        out.println("<p>password: " + password + "</p>");
-    /*    out.println("</body></html>"); */
+        out.println("This resource is not available directly.");
     }
 
     protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
-        /* From Hibernate example */
-        Map<String, Object> myMap = new HashMap<String, Object>();
         final Session session = getSession();
         try {
             System.out.println("querying all the managed entities...");
@@ -70,19 +54,13 @@ public class Servlet extends javax.servlet.http.HttpServlet {
                 for (Object o : query.list()) {
                     QuestionEntity qe = (QuestionEntity) o;
                     System.out.println("Id: " + qe.getId());
-                    // Hibernate example: System.out.println("Question: " + qe.getQuestion());
                     /* Added to test */
                     PrintWriter out = response.getWriter();
                     response.setContentType("text/html");
                     out.println(qe.getQuestion());
                     ObjectMapper objectMapper = new ObjectMapper(); //Secret sauce!
                     try {
-                        //myMap = objectMapper.readValue(qe.getQuestion(), HashMap.class);
                         Question question = objectMapper.readValue(qe.getQuestion(), Question.class);
-                        /*
-                        out.println("Question object: " + question);
-                        out.println("Question attribute: " + question.getQuestion());
-                         */
                         ArrayList qOpts = question.getIncorrect_answers();
                         String strOptions = shuffleOptions(qOpts, question.getCorrect_answer());
                         request.setAttribute("options", strOptions);
