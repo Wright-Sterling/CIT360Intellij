1707ab478afa5af11f449931b7877fb00b3600eb
Wright-Sterling <j.sterling.wright@gmail.com>
2019-04-11 13:06:41 -0400

    Attempted but failed to use local storage for running total.

--- MyQuiz360/web/quiz.js
+++ MyQuiz360/web/quiz.js
@@ -2,10 +2,21 @@ var runningScore = 0; // store and retrieve from local storage
 var questionValue = 0; // will be updated to match difficulty
 var newValue = 0;
 var qButton = document.getElementsByClassName("question-button")[0];
-var buttonText = ["Ready...", "Set...", "Go!"];
 var downloadTimer = null;
 var canvas =  document.querySelector("canvas");
-var ctx = canvas.getContext("2d");
+var supportLocalStorage;
+
+if (typeof (Storage) === "undefined") {
+    alert("Your browser does not support local storage. Your score will not persist between questions.");
+    supportLocalStorage = false;
+}
+
+if (supportLocalStorage) {
+    runningScore = parseInt(localStorage.runningScore);
+    if (isNaN(runningScore)) {
+        runningScore = 0;
+    }
+}
 
 updateRunningScore();
 
@@ -17,111 +28,11 @@ function updateRunningScore() {
     }
 }
 
-function showQuestion() {
-    question = quizArray.results[upNext];
-    switch (question.difficulty) {
-        case "easy":
-            questionValue = 400;
-            break;
-        case "medium":
-            questionValue = 600;
-            break;
-        case "hard":
-            questionValue = 1000;
-            break;
-        default:
-            questionValue = 200; //should never be this but...
-    }
-    upNext++;
-    if (upNext == quizArray.results.length) {
-        upNext = 0; // just loop for now. Go get more questions eventually
-        // in fact, this should be a while loop like a normal game
-    }
-    document.getElementById("question").innerHTML = "";
-    document.getElementById("category").innerHTML =
-        "Category: " + question.category;
-    document.querySelector("#answer").innerHTML = "Answer:";
-    document.querySelector(".correct-value").innerHTML = questionValue;
-    document.querySelector(".correct-title").classList.add("pulse");
-    document.querySelector(".correct-value").classList.add("pulse");
-
-    ctx.clearRect(0,0,canvas.width,canvas.height);
-    qButton.innerHTML = buttonText[0];
-    var y = 0;
-    setTimeout(myTimeout1, 1500);
-    setTimeout(myTimeout2, 3000);
-    setTimeout(myTimeout3, 3500);
-}
-
-function myTimeout1() {
-    qButton.innerHTML = buttonText[1];
-}
-function myTimeout2() {
-    qButton.innerHTML = buttonText[2];
-}
-function myTimeout3() {
-    displayQuestion(question);
-    /* This is a good place to remove the animation class so the effect will work next time */
-    document.querySelector(".correct-title").classList.remove("pulse");
-    document.querySelector(".correct-value").classList.remove("pulse");
-}
-
-function valueCountdown() {
-    newValue = questionValue;
-    downloadTimer = setInterval(function(){
-        var correctValueElement = document.getElementsByClassName("correct-value")[0];
-        var incorrectValueElement = document.getElementsByClassName("incorrect-value")[0];
-        newValue -= 50;
-        correctValueElement.innerHTML = newValue;
-        incorrectValueElement.innerHTML = (questionValue - newValue) * -1;
-        if(newValue <= 0){ // testing here rather than at top on purpose
-            clearInterval(downloadTimer);
-            correctValueElement.innerHTML = 0;
-            incorrectValueElement.innerHTML = questionValue * -1;
-        }
-    }, 1000);
-}
-
-function showAnswer() {
-    if (showingAnswer === true) {
-        document.querySelector("#answer").classList.remove("show");
-        showingAnswer = false;
-    } else {
-        document.querySelector("#answer").classList.add("show");
-        showingAnswer = true;
-    }
-}
-
-function displayQuestion(nextQuestion) {
-    var strOptions = "";
-    var qQuest = question.question;
-    var qOpts = question.incorrect_answers;
-    qOpts.push(question.correct_answer);
-    shuffledOptions = qOpts.sort(function(a, b){return 0.5 - Math.random()}); // shuffle array
-
-    valueCountdown();
-    for (i = 0; i < qOpts.length; i++) {
-        var strOptions = strOptions +
-            "<p>"+
-            "<input type='radio' id='option"+i+"' name='radio-group'"+
-            "onclick='getAnswer(this.id)'>"+
-            "<label for='option"+i+"'>"+qOpts[i]+"</label>"+
-            "</p>"
-    }
-    document.querySelector("#options").className="show";
-    document.getElementById("question").innerHTML = qQuest;
-    document.getElementById("options").innerHTML = strOptions;
-    document.getElementById("answer").innerHTML = "Answer: " + question.correct_answer;
-}
-
 function getAnswer(answer) {
-    clearInterval(downloadTimer);
     var numAnswer = answer.slice(6);
-    var selectedButton = document.getElementById(answer);
     var selectedLabel = document.querySelector("label[for=option"+numAnswer+"]");
-    var feedbackText = document.querySelector("#feedback");
     var optionsID = document.querySelector("#options");
-    var style = window.getComputedStyle ? getComputedStyle(optionsID) : optionsID.curentStyle;
+    var style = window.getComputedStyle ? getComputedStyle(optionsID) : optionsID.currentStyle;
     var optionsArea = {
         marginLeft: parseInt(style.marginLeft) || 0,
         offsetTop: optionsID.offsetTop,
@@ -139,12 +50,12 @@ function getAnswer(answer) {
         selectedLabel.style.color = "green";
         ctx.fillStyle = "green";
         ctx.fillText("CORRECT!", canvas.width/2, canvas.height/2);
-        runningScore += newValue;
+        runningScore += parseInt(document.querySelector(".correct-value").innerHTML);
     } else {
         selectedLabel.style.color = "red";
         ctx.fillStyle = "red";
         ctx.fillText("INCORRECT!", canvas.width/2, canvas.height/2);
-        runningScore -= questionValue - newValue;
+        runningScore += parseInt(document.querySelector(".incorrect-value").innerHTML);
     }
     optionsID.className="hide";
     qButton.innerHTML="New Question";
